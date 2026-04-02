a2f4a444e979aadd3ef8a5bb839d3de51a03fa10
therealthori <2570507@students.wits.ac.za>
2026-04-02 11:45:06 +0200

    Health Bar and part of the Object Logic working

---
+++ Assets/ThoriScript/Billboard.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform cam;
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
